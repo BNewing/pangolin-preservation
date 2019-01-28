ad2aceccd8872bbbc9b2c2e468a73ab3c5ee9bbf
BNewing <bnewing92@hotmail.co.uk>
2019-01-28 17:39:11 +0000

    Add pangolin gif entry form and entry display pages
    You can now enter details about a gif into the form, be shown that once it's entered and then go to an index page that shows all the gifs you've entered so far.

---
+++ app/controllers/gifs_controller.rb
@@ -0,0 +1,24 @@
+class GifsController < ApplicationController
+	def index 
+		@gifs = Gif.all
+	end
+
+	def show
+		@gif = Gif.find(params[:id])
+	end	
+
+	def new
+	end
+
+	def create
+		@gif = Gif.new(gif_params)
+
+		@gif.save
+		redirect_to @gif
+	end
+
+	private
+		def gif_params
+			params.require(:gif).permit(:description, :url)
+		end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :gifs
+
+  root 'welcome#index'
+end
